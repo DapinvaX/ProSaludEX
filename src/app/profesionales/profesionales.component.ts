d1f1b33877f7467dc349d61d6317c52b23e2e8b3
DapinvaX <dapinva95@gmail.com>
2023-06-16 10:36:43 +0200

    Ultimos Cambios

--- src/app/profesionales/profesionales.component.ts
+++ src/app/profesionales/profesionales.component.ts
@@ -6,10 +6,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profesionales.component.css']
 })
 export class ProfesionalesComponent implements OnInit {
+  json = {
+    "medicos": [
+      {
+        "id": 1,
+        "num_colegiado": "123456789",
+        "nombre": "Juan",
+        "apellido": "Pérez",
+        "apellido2": "López",
+        "especialidad": "Traumatología",
+        "usuario": "juan",
+        "contrasena": "juan1234"
+      },
+      {
+        "id": 2,
+        "num_colegiado": "123456799",
+        "nombre": "María",
+        "apellido": "García",
+        "apellido2": "López",
+        "especialidad": "Medicina General",
+        "usuario": "maria",
+        "contrasena": "maria1234"
+      },
+      {
+        "id": 3,
+        "num_colegiado": "143456799",
+        "nombre": "Antonio",
+        "apellido": "Ramirez",
+        "apellido2": "Castro",
+        "especialidad": "Neurología",
+        "usuario": "antonio",
+        "contrasena": "antonio1234"
+      },
+      {
+        "id": 4,
+        "num_colegiado": "123556799",
+        "nombre": "María",
+        "apellido": "García",
+        "apellido2": "López",
+        "especialidad": "Neumología",
+        "usuario": "maria",
+        "contrasena": "maria1234"
+      }
+    ]
+  };
+
+  //Damos una variable al json
+ profesionales = this.json.medicos;
+
+
+  //Y aqui lo metemos en un array
+  profesionalesArray: any[] = [this.profesionales];
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.profesionales = this.profesionalesArray[0];
   }
 
 }
